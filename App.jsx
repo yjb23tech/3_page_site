f93cac79c642c903e81defe6944a5b7d24b1bed8
yjb23tech <yjb23tech@gmail.com>
2023-12-29 23:33:43 +0000

    More and More

--- App.jsx
+++ App.jsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client"
+import NavHeader from "./components/NavHeader"
 
 const App = () => {
     return (
         <div>
-
+            <NavHeader nav_header_text="Earth and Mars" />
         </div>
     )
 }
