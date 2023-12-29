28ab38e26d2a88fb5d3eba3f12f0608091e41640
yjb23tech <yjb23tech@gmail.com>
2023-12-29 23:46:56 +0000

    Client Server Model

--- App.jsx
+++ App.jsx
@@ -7,6 +7,9 @@ const App = () => {
             <NavHeader nav_header_text="Earth and Mars" />
             <p>Earth and Mars are two planets within our solar system</p>
             <p>Which planet would you like to know better?</p>
+
+            <p>I would like to <a href="./earth.html">know more about Earth</a></p>
+            <p>I would like to <a href="./mars.html">know more about Mars</a></p>
         </div>
     )
 }
