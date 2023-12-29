7fbe8d325547a3cdf52b184f5e3c51c61857e47d
yjb23tech <yjb23tech@gmail.com>
2023-12-29 23:42:25 +0000

    At the heart of this is simplicity

--- App.jsx
+++ App.jsx
@@ -5,6 +5,11 @@ const App = () => {
     return (
         <div>
             <NavHeader nav_header_text="Earth and Mars" />
+            <p>Earth and Mars are two planets within our solar system</p>
+            <p>Which planet would you like to know better?</p>
+
+            <p>I would like to <a href="./earth.html">know more about <p style="color: green">Earth</p></a></p>
+            <p>I would like to <a href="./mars.html">know more about <p style="color: red">Mars</p></a></p>
         </div>
     )
 }
