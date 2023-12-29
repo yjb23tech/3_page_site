d0a6883932908191282a9c5c22a9d918239b3367
yjb23tech <yjb23tech@gmail.com>
2023-12-29 23:43:40 +0000

    Try again

--- App.jsx
+++ App.jsx
@@ -7,9 +7,6 @@ const App = () => {
             <NavHeader nav_header_text="Earth and Mars" />
             <p>Earth and Mars are two planets within our solar system</p>
             <p>Which planet would you like to know better?</p>
-
-            <p>I would like to <a href="./earth.html">know more about <p style="color: green">Earth</p></a></p>
-            <p>I would like to <a href="./mars.html">know more about <p style="color: red">Mars</p></a></p>
         </div>
     )
 }
